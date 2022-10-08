45fb76052c59dcbfdfc8756666f5ce339a95c0d6
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-08 10:47:14 +0100

    initialized firebase

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDawN0zKQB5H8u65LhSqUedo-m6FNY8In0",
+    authDomain: "linkedin-clone-788c4.firebaseapp.com",
+    projectId: "linkedin-clone-788c4",
+    storageBucket: "linkedin-clone-788c4.appspot.com",
+    messagingSenderId: "242067231610",
+    appId: "1:242067231610:web:db859424dddd6cbf623cc3",
+    measurementId: "G-3NNZHKHC45"
+  };
+  
+const app = initializeApp(firebaseConfig);
