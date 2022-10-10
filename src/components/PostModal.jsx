193259a320ce25a94e621d43e36d92643638f375
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-10 20:21:42 +0100

    adding a new feature

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -5,7 +5,29 @@ function PostModal() {
   return (
    <Container>
        <Content>
-           Coneten
+           <Header>
+              <h2> Create Post</h2>
+              <button>
+                  Close
+              </button>
+           </Header>
+           <SharedContent>
+               <UserInfo>
+                 <img src='/images/user.svg' alt="user"/>
+                  <span> Name </span>
+               </UserInfo>
+           </SharedContent>
+           <ShareImage>
+              <AttachAssets>
+                  <AssetsButton>
+                      <img src="/images/photo-upload-icon.svg" alt=""/>
+                  </AssetsButton>
+
+                  <AssetsButton>
+                  <img src="/images/video-upload-icon.svg" alt=""/>
+                  </AssetsButton>
+              </AttachAssets>
+           </ShareImage>
        </Content>
    </Container>
   )
@@ -34,5 +56,74 @@ const Content =  styled.div`
    max-height: 90%;
    top : 2rem ;
 `
+
+const Header = styled.div`
+    display: block;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding : 16px 20px ;
+    line-height: 1.5 ;
+
+    button {
+         border: none;
+         outline: none;
+         background: transparent;
+         height: 40px;
+         width: 40px;
+         min-width: auto;
+    }
+`
+
+
+const SharedContent = styled.div`
+    display:flex;
+    flex-direction: column;
+    flex-grow: 1;
+    overflow-y: auto;
+    padding : 12px 16px ;
+    `
+
+
+const UserInfo  = styled.div`
+     display:  flex;
+     align-items: center;
+    padding : 12px 24px ;
+
+    svg , img {
+         border-radius: 50%;
+         width: 50px;
+         height: 50px;
+    }
+
+    span {
+         margin-left : 8px;
+    }
+`
+
+
+const ShareImage = styled.div`
+     display: flex;
+     justify-content: space-between;
+     padding : 12px 24px 12px 16px;
+`
+
+const AssetsButton = styled.button`
+   display : flex ;
+   align-items: center;
+   height: 40px;
+   max-width: auto;
+`
+const AttachAssets = styled.div`
+display:  flex;
+align-items: center;
+padding-right: 8px ;
+
+  ${AssetsButton} {
+      width:40px;
+}
+`
+
+
 
 export default PostModal
