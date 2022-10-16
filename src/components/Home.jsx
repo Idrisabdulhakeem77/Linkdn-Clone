2e22477e757df0377a15eb25cb7cdf079ba8b3df
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-16 08:25:18 +0100

    updated readme file

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,14 @@
 import styled from "styled-components"
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Navigate } from "react-router-dom"
 import { useDispatch , useSelector } from "react-redux"
 import { signInApi } from "../redux/userSlice"
-// import { signInApi } from "../redux/Redux"
-import { signIn } from "../redux/userSlice"
 
 
 
 const  Home = () =>  {
     const user =  useSelector(state => state.user)
    const dispatch = useDispatch()
-   const navigate = useNavigate()
 
   return (
   
