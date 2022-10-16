22c65d11876183644f5e2e8352683eb7cb3efca6
Idrisabdulhakeem77 <idrisabdulhakeem77@gmail.com>
2022-10-16 08:13:31 +0100

    fixed some errors

--- src/components/PostModal.jsx
+++ src/components/PostModal.jsx
@@ -1,7 +1,7 @@
 import React  , {useState } from 'react'
 import { useSelector , useDispatch } from 'react-redux'
 import styled from 'styled-components'
-// import { postArticleApi } from '../redux/action'
+
 import { Timestamp } from 'firebase/firestore/lite'
 import {postArticleApi} from '../redux/userSlice'
 
